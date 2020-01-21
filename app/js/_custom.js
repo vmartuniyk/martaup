871570ebe3ec24834b5b3dba5ac58f8486421172
Volodimir Martuniyk <volodimir.martuniyk@gmail.com>
2020-01-21 23:21:33 +0100

    Додання слайдер. Частина1

--- app/js/_custom.js
+++ app/js/_custom.js
@@ -1,5 +1,10 @@
-document.addEventListener("DOMContentLoaded", function() {
+$(function() {
 
-	// Custom JS
+	$('.header-slider').slick({
+        arrows: false,
+        vertical: true,
+        dots: true,
+        dotsClass: 'header-dots'
+    });
 
 });
